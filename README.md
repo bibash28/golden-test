2a574e3a14237dff7604da66a63a702f5cd3c4af
Bibash Shrestha <bibashshrestha@gmail.com>
2022-03-29 17:37:22 +0545

    first commit - sunflower UI added

---
+++ README.md
@@ -0,0 +1,3 @@
+# golden_test
+ 
+- [Golden Test Tutorial](https://verygood.ventures/blog/alchemist-golden-tests-tutorial)
---
+++ android/app/src/main/kotlin/com/bibash28/golden_test/golden_test/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.bibash28.golden_test.golden_test
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
